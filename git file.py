2bb084bdce3ee9486ecfa7d2f4323e009d1673f1
kelvinquaicoe <kelvinquaicoe20@gmail.com>
2025-09-24 11:41:01 -0400

    kkquaicoe_assignment_3.py

--- git file.py
+++ git file.py
@@ -44,4 +44,43 @@ else:
 
 #Pass Test Case 2: Course planning with if/elif/else and comparison operators
 
+subject= ["Programming", "Math", "English", "History"]
+
+sub= input('Enter study option: ')
+if sub == "Programming":
+    if current_gpa < 2.0 and social_points > 50:
+        social_points-= 20
+    elif current_gpa < 3.0 and social_points > 50:
+        social_points -= 10
+    elif current_gpa >= 3.5 or stress_level > 80:
+        social_points += 30
+elif sub == "Math":
+    if current_gpa < 2.0 and social_points > 50:
+        social_points-= 20
+    elif current_gpa < 3.0 and social_points > 50:
+        social_points -= 10
+    elif current_gpa >= 3.5 or stress_level > 80:
+        social_points += 20
+elif sub == "English":
+    if current_gpa < 2.0 and social_points > 50:
+        social_points-= 5
+    elif current_gpa < 3.0 and social_points > 50:
+        social_points -= 1
+    elif current_gpa >= 3.5 or stress_level > 80:
+        social_points += 15
+elif sub == "History":
+    if current_gpa < 2.0 and social_points > 50:
+        social_points-= 5
+    elif current_gpa < 3.0 and social_points > 50:
+        social_points -= 1
+    elif current_gpa >= 3.5 or stress_level > 80:
+        social_points += 10
+elif sub not in subject  :
+    print('Subject chosen is not available')
+
+print(f'Social points have decreased/incresaed to: {social_points}')
+
+#Pass Test Case 3: Study strategy with membership and logical operators
+
+
 
